4f854d49ff80897f640c222869e7993de423bd49
MajedDev <alribimajed@gmail.com>
2024-03-02 13:08:01 +0300

    Axe Enemy and prefabs

---
+++ Assets/Scripts/EnemiesSpawn.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemiesSpawn : MonoBehaviour
+{
+    [SerializeField] private  Transform[] spawnPoints = new Transform[4];
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    Transform targetTree;
+    [SerializeField] private float movementSpeed = .1f;
+    [SerializeField] private float attackRange = .5f;
+    Animator anim;
+    bool facingRight = true;
+
+    //Boolean Phases
+    private bool isAttacking = false;
+    void Start()
+    {
+        targetTree = GameObject.FindGameObjectWithTag("Tree").GetComponent<Transform>();
+        anim = GetComponentInChildren<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        chaseTree();
+        Attacking();
+        anim.SetBool("isAttacking", isAttacking);
+    }
+    private void chaseTree()
+    {
+        if(!isAttacking)
+        transform.position= Vector3.MoveTowards(transform.position,targetTree.transform.position,movementSpeed * Time.deltaTime);
+
+        if (transform.position.x > targetTree.transform.position.x && facingRight)
+        {
+            transform.Rotate(0, 180, 0);
+            facingRight = !facingRight;
+        }
+        else
+        {
+            transform.Rotate(0, 0, 0);
+        }
+    }
+    private void Attacking()
+    {
+        if(Vector3.Distance(transform.position,targetTree.transform.position) <= attackRange)
+        {
+            isAttacking = true;
+
+        }
+    }
+}
